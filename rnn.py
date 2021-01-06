3780e2331c48da3c405c3ddec0e56c1d681915d8
allenwu <allenntuee@gmail.com>
2021-01-06 17:26:09 +0800

    add pseudo code in rnn.py

--- rnn.py
+++ rnn.py
@@ -1,9 +1,25 @@
 
 import tensorflow as tf
-import numpy
+import numpy as np
 
 
 def rnnModel():
     pass
 
 def trainModel():
+    pass
+
+def testModel():
+    pass
+
+
+if __name__ == '__main__':
+    
+    ## data preprocessing
+
+    ## build model
+
+    ## training
+
+    ## testing
+    pass
