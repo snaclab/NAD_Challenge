75b94873ff0b187dc3f802cbc88858ccd0c8bde4
ZixinYang <zixinyang92@gmail.com>
2021-02-09 15:04:13 +0800

    ouput probabilities instead of predicted classes

--- learning.py
+++ learning.py
@@ -3,8 +3,6 @@ import pandas as pd
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from collections import Counter
-
 
 def eval(Y_test, y_pred):
     print('Test set:')
@@ -15,7 +13,7 @@ def XGB_training(data, n_class):
     X = data[[c for c in data.columns if c != 'label']].copy()
     Y = data[['label']].copy()
     X_train, X_test, y_train, y_test = train_test_split(X.values, Y.values, test_size=0.2, random_state=7)
-    model = XGBClassifier(objective='multi:softmax', num_class=n_class, learning_rate=0.1, use_label_encoder=False)
+    model = XGBClassifier(objective='multi:softprob', num_class=n_class, learning_rate=0.1, use_label_encoder=False)
     eval_set = [(X_train, y_train), (X_test, y_test)]
     model.fit(X_train, y_train, eval_metric=["merror", "mlogloss"], eval_set=eval_set, verbose=True, early_stopping_rounds=10)
     print('Validation set:')
@@ -25,4 +23,4 @@ def XGB_training(data, n_class):
 
 def XGB_prediction(data, model):
     X_test = data[[c for c in data.columns if c != 'label']].copy().values
-    return model.predict(X_test)
+    return model.predict_proba(X_test)
