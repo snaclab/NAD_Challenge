63f432f3f9c0df0a54e4cd9b02ee7880ca324c89
ZixinYang <zixinyang92@gmail.com>
2021-02-18 14:55:16 +0800

    add an argument

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 virtualenv -p $(which python3) 156_venv
 . 156_venv/bin/activate
 python -m pip install -r requirements.txt
-#python preprocess.py --trn 1203_firewall.csv 1210_firewall.csv 1216_firewall.csv --tst 156_SNACLab_1_0123_firewall.csv 156_SNACLab_1_0124_firewall.csv 156_SNACLab_1_0125_firewall.csv 156_SNACLab_1_0126_firewall.csv --output_trn train.csv --pretrained 1
-#python main.py --trn train.csv --tst_src 156_SNACLab_1_0123_firewall.csv 156_SNACLab_1_0124_firewall.csv 156_SNACLab_1_0125_firewall.csv 156_SNACLab_1_0126_firewall.csv --pretrained 1
+#python preprocess.py --trn 1203_firewall.csv 1210_firewall.csv 1216_firewall.csv --tst 156_SNACLab_1_0123_firewall.csv 156_SNACLab_1_0124_firewall.csv 156_SNACLab_1_0125_firewall.csv 156_SNACLab_1_0126_firewall.csv --output_trn train.csv --pretrained True --eval False
+#python main.py --trn train.csv --tst_src 156_SNACLab_1_0123_firewall.csv 156_SNACLab_1_0124_firewall.csv 156_SNACLab_1_0125_firewall.csv 156_SNACLab_1_0126_firewall.csv --pretrained True --eval False
 
-python preprocess.py --trn ../nad/train_no.csv --tst ../nad/test_no.csv --output_trn train.csv 
+python preprocess.py --trn ../nad/train_no.csv --tst ../nad/test_no.csv --output_trn train.csv
 python main.py --trn train.csv --tst_src ../nad/test_no.csv
 
 deactivate
