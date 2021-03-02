bb3cadd624af756fa83ead9a857d41c7c02ac88f
Zixin Yang <zixinyang92@gmail.com>
2021-03-02 03:21:39 +0000

    modify post processing / adjust time period

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import datetime
 from sklearn.metrics import fbeta_score
 import math
 from sklearn.metrics import confusion_matrix
-from ensembling import ensemble
 
 def parse_arg():
     parser = argparse.ArgumentParser(description='Process dataset name')
@@ -29,10 +28,6 @@ def evaluation(data_tst, y_pred):
     cost_matrix = np.array([[0,2,1,1,1],[2,0,1,1,1],[2,1,0,1,1],[2,1,1,0,1],[2,1,1,1,0]])
     conf_matrix = confusion_matrix(Y_test, y_pred)
     cost = np.multiply(cost_matrix, conf_matrix)
-    print('cost matrix: ',cost)
-    print('total cost: ',np.sum(cost))
-    print('max cost: ',np.amax(cost))
-    print('log total / log max: ', math.log(np.sum(cost))/math.log(np.amax(cost)))
     print('Evaluation criteria: ', 0.3*(1-(math.log(np.sum(cost))/math.log(np.amax(cost))))+0.7*macro_fbeta_score)
 
 if __name__ == '__main__':
@@ -52,41 +47,40 @@ if __name__ == '__main__':
         # predictions
         y_pred = learning.XGB_prediction(data_tst, model)
         df_pred = pd.DataFrame(columns=[0,1,2,3,4], data=y_pred)
-        # postprocess
-        test = pd.read_csv(tst_file)
-        ans = test[['time','src']].copy()
-        ans = pd.concat([ans, df_pred], axis=1)
-        ans['time'] = ans['time'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-        ans['time'] = ans['time'].apply(lambda x: str(x.month).zfill(2)+str(x.day).zfill(2)+str(x.hour).zfill(2)+str(x.minute).zfill(2))
-        ans['time+src'] = ans['time'].apply(str) + ans['src'].apply(str)
-        ans.drop(columns=['time','src'],inplace=True)
-        d_v = ans.groupby(['time+src']).sum()
-        d_v = d_v.idxmax(axis='columns').to_frame()
-        DIC = {}
-        for idx, row in d_v.iterrows():
-            DIC[idx] = row[0]
-        ans['pred'] = [1 for i in range(len(ans))]
-        ans['pred'] = ans['time+src'].apply(lambda x: DIC[x])
-        y_pred = ans['pred'].values
-        
-        # Transform label and save data
-        label_map = {0: 'DDOS-smurf', 1: 'Normal', 2: 'Probing-IP sweep', 3: 'Probing-Nmap', 4: 'Probing-Port sweep'}
-        test['label'] = ans['pred']
-        test['label'] = test['label'].apply(lambda x: label_map[x])
-
-        if str(args.eval)=='True':
-            evaluation(data_tst, y_pred)
-        test.to_csv(tst_file, index=False)
-        
-        #ensemble('xgb', args.eval, tst_file, tst_file[:-4]+'_xgb.csv', tst_file[:-4]+'_nn.csv')
-        f_name = list(data_tst.columns)
-        f_name.remove('label')
-        model.get_booster().feature_names = f_name
-        fi = model.get_booster().get_score(importance_type="gain")
-        print({k: v for k, v in sorted(fi.items(), key=lambda item: item[1], reverse=True)})
-    '''
-    # plot feature importance
-    model.get_booster().feature_names = list(data_trn.columns)[:-1]
-    fig, ax = plt.subplots(figsize=(10, 10))
-    plot_importance(model.get_booster(), ax)
-    '''
+        for time_setting in ['minute', 'hour']:
+            # postprocess
+            test = pd.read_csv(tst_file)
+            ans = test[['time','src']].copy()
+            ans = pd.concat([ans, df_pred], axis=1)
+            ans['time'] = ans['time'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+            if time_setting == 'hour':
+                ans['time'] = ans['time'].apply(lambda x: str(x.month).zfill(2)+str(x.day).zfill(2)+str(x.hour).zfill(2))
+            elif time_setting == 'minute':
+                ans['time'] = ans['time'].apply(lambda x: str(x.month).zfill(2)+str(x.day).zfill(2)+str(x.hour).zfill(2)+str(x.minute).zfill(2))
+            ans['time+src'] = ans['time'].apply(str) + ans['src'].apply(str)
+            ans.drop(columns=['time','src'],inplace=True)
+            d_v = ans.groupby(['time+src']).sum()
+            d_v = d_v.idxmax(axis='columns').to_frame()
+            DIC = {}
+            for idx, row in d_v.iterrows():
+                DIC[idx] = row[0]
+            ans['pred'] = [1 for i in range(len(ans))]
+            ans['pred'] = ans['time+src'].apply(lambda x: DIC[x])
+            if time_setting == 'hour':
+                m_df = pd.read_csv(tst_file[:-4]+'_minute_predicted.csv')
+                d_idx = list(m_df[m_df['label']=='DDOS-smurf'].index)
+                ans.loc[d_idx, 'pred'] = 0
+            y_pred = ans['pred'].values
+            
+            # Transform label and save data
+            label_map = {0: 'DDOS-smurf', 1: 'Normal', 2: 'Probing-IP sweep', 3: 'Probing-Nmap', 4: 'Probing-Port sweep'}
+            test['label'] = ans['pred']
+            test['label'] = test['label'].apply(lambda x: label_map[x])
+            if str(args.eval)=='True':
+                # Evaluation
+                evaluation(data_tst.copy(), y_pred)
+            else:
+                if time_setting == 'minute':
+                    test.to_csv(tst_file[:-4]+'_'+time_setting+'_predicted.csv', index=False)
+                elif time_setting == 'hour':
+                    test.to_csv(tst_file, index=False)
